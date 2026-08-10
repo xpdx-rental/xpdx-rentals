daeea7f8ea5054f8495175a2d232f4cbb9026718
Bikash <ybikash919@gmail.com>
2026-08-10 23:22:33 +0530

    chore: update google site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -116,7 +116,7 @@ export const metadata: Metadata = {
     },
   },
   verification: {
-    google: "1_k8pvVl2rvJULkNXMUoYK2xo7s0yGZcpNkndN_qKIk",
+    google: "1qP_fQ1-OtwX-Ik4LYtwDmYq5AnVIJptZj6TD-JIxIM",
   },
 };
 
